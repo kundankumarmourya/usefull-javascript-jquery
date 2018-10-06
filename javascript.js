7e76aad9ec727efe1b911acba99cb28dcd7cbec5
Kundan Kumar Mourya <32189172+kundankumarmourya@users.noreply.github.com>
2018-10-06 12:53:04 +0530

    Fill add

---
+++ javascript.js
@@ -0,0 +1,53 @@
+
+
+            $(document).ready(function ($) {
+                if ($('.navbar').length >= 1){
+                // grab the initial top offset of the navigation 
+                    var stickyNavTop = $('.navbar').offset().top;
+                    // our function that decides weather the navigation bar should have "fixed" css position or not.
+                    var stickyNav = function () {
+                        var scrollTop = $(window).scrollTop(); // our current vertical position from the top
+                        // if we've scrolled more than the navigation, change its position to fixed to stick to top,
+                        // otherwise change it back to relative
+                        if (scrollTop > stickyNavTop) {
+                            $('.navbar').addClass('sticky');
+                            $('.navbar-brand').addClass('blok');
+                            $('.navbar-brand').removeClass('non');
+                        } else {
+                            $('.navbar').removeClass('sticky');
+                            $('.navbar-brand').addClass('non');
+                            $('.navbar-brand').removeClass('blok');
+                        }
+                    };
+                    stickyNav();
+                    // and run it again every time you scroll
+                    $(window).scroll(function () {
+                        stickyNav();
+                    });
+                }
+                $(".ab").click(function () {
+
+                    $('.coverFull').height($(document).height());
+
+                    $('.coverFull').fadeToggle();
+                    $('.coverFull').css({ display: "block" });
+                });
+            });
+
+
+     
+            $(function () {
+                $('a[href*=#]:not([href=#])').click(function () {
+                    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                        var target = $(this.hash);
+                        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                        if (target.length) {
+                            $('html,body').animate({
+                                scrollTop: target.offset().top
+                            }, 1000);
+                            return false;
+                        }
+                    }
+                });
+            });
+    
